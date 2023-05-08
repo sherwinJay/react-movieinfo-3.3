a25600f2eee9aa4ca1c5d377c3afaa68855d8513
sherwin.web.ph <sherwin.tripleonetech@gmail.com>
2023-05-08 17:12:25 +0800

    test

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -18,15 +18,15 @@ const Movies = async ({params}: {params: {id: string}}) => {
       /> */}
       <div className="w-[100%] m-auto p-[1.5em] grid grid-cols-1 grid-customRow3 gap-[1.5em] lg:grid-cols-4 lg:grid-rows-2 lg:p-[3em] xl:w-[1200px]">
         <div className="lg:col-start-1 lg:col-end-4 lg:row-start-1 lg:row-end-2 lg:h-auto">
-          <LeadCast
+          {/* <LeadCast
             castData={movieList}
-          />
+          /> */}
         </div>
         <div className="lg:col-start-4 lg:col-end-5 lg:row-start-1 lg:row-end-3">
-          <Sidebar
+          {/* <Sidebar
             isMovie={true}
             contentData={movieList}
-          />
+          /> */}
         </div>
         <div className="lg:row-start-2 lg:row-end-3 lg:col-start-1 lg:col-end-4">
           {/* <RecommendMovies
