5fe248a65d3bf123300059606cc80ef0b8169b14
sherwin.web.ph <sherwin.tripleonetech@gmail.com>
2023-05-11 23:21:40 +0800

    test update

--- hooks/fetchRecommendMovies.ts
+++ hooks/fetchRecommendMovies.ts
@@ -1,13 +1,19 @@
 import { movieDbURL } from "@/constant";
 
 export async function fetchRecommendMovies(pageId: string | number, mediaType: string){
-  const res = await fetch(
-    `${movieDbURL}/3/${mediaType}/${pageId}/recommendations?api_key=${process.env.MOVIE_DATABASE_ID}&language=en-US&page=1`,
-    {cache: "no-store"}
-  );
-  const recommendMovieList = await res.json()
 
-  return recommendMovieList
+  try {
+    const res = await fetch(
+      `${movieDbURL}/3/${mediaType}/${pageId}/recommendations?api_key=${process.env.MOVIE_DATABASE_ID}&language=en-US&page=1`,
+      {cache: "no-store"}
+    );
+    const recommendMovieList = await res.json()
+  
+    return recommendMovieList
+  
+  } catch (error) {
+    
+  }
 }
 
 // const useFetch = function<T>(url: string) {
--- hooks/getMovieData.ts
+++ hooks/getMovieData.ts
@@ -1,12 +1,16 @@
 import { movieDbURL } from "@/constant";
 
 export async function getMovieData(pageId: string | number, mediaType: string){
-  const res = await fetch(
-    `${movieDbURL}/3/${mediaType}/${pageId}?api_key=${process.env.MOVIE_DATABASE_ID}&append_to_response=credits,videos`,
-      {cache: "no-store"}
-  );
+  try {
+    const res = await fetch(
+      `${movieDbURL}/3/${mediaType}/${pageId}?api_key=${process.env.MOVIE_DATABASE_ID}&append_to_response=credits,videos`,
+        {cache: "no-store"}
+    );
+    
+    const movieList = await res.json()
   
-  const movieList = await res.json()
-
-  return movieList
+    return movieList
+  } catch (error) {
+    // console.log(error)
+  }
 }
