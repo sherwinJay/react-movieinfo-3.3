2536d63affd1540574cc6b43c5a314578a0edacb
sherwin.web.ph <sherwin.tripleonetech@gmail.com>
2023-05-19 17:33:26 +0800

    test update 12

--- app/components/Sidebar/index.tsx
+++ app/components/Sidebar/index.tsx
@@ -22,15 +22,15 @@ const Sidebar = ({isMovie, contentData}: Props) => {
   const slicedCrew = credits?.crew?.slice(0,4);
 
   const getCompanies = slicedProductionCompany?.map((company) => (
-    <p className="" key={company.id}>
+    <p className="text-[14px] md:text-[18px]" key={company.id}>
       {company.name}
     </p>
   ))
   
   const getCrew = slicedCrew?.map((crew) => (
     <div key={crew.credit_id}>
-      <p className="leading-5">{crew.name}</p>
-      <p className="text-[13px] italic">{crew.job}</p>
+      <p className="text-[14px] md:text-[18px] leading-5">{crew.name}</p>
+      <p className="text-[11px] md:text-[13px] italic">{crew.job}</p>
     </div>
   ))
 
@@ -41,28 +41,28 @@ const Sidebar = ({isMovie, contentData}: Props) => {
         isMovie ? (
           <>
             <div>
-              <h4 className="text-[#c9173d] font-bold text-[18px]">Budget</h4>
-              <p>{`$ ${budget !== "" ? budget?.toLocaleString() : '-'}`}</p>
+              <h4 className="text-[#c9173d] font-bold text-[16px] md:text-[18px]">Budget</h4>
+              <p className="text-[14px] md:text-[18px]">{`$ ${budget !== "" ? budget?.toLocaleString() : '-'}`}</p>
             </div>
             <div>
-              <h4 className="text-[#c9173d] font-bold text-[18px]">Revenue</h4>
-              <p>{`$ ${revenue !== "" ? revenue?.toLocaleString(): '-'}`}</p>
+              <h4 className="text-[#c9173d] font-bold text-[16px] md:text-[18px]">Revenue</h4>
+              <p className="text-[14px] md:text-[18px]">{`$ ${revenue !== "" ? revenue?.toLocaleString(): '-'}`}</p>
             </div>
           </>
         ) : (
           <div>
-            <h4 className="text-[#c9173d] font-bold text-[18px]">Original Name</h4>
-            <p>{original_name}</p>
+            <h4 className="text-[#c9173d] font-bold text-[16px] md:text-[18px]">Original Name</h4>
+            <p className="text-[14px] md:text-[18px]">{original_name}</p>
           </div>
         )
       }
 
       <div>
-        <h4 className="text-[#c9173d] font-bold text-[18px]">Status</h4>
-        <p>{status}</p>
+        <h4 className="text-[#c9173d] font-bold text-[16px] md:text-[18px]">Status</h4>
+        <p className="text-[14px] md:text-[18px]">{status}</p>
       </div>
       <div>
-        <h4 className="text-[#c9173d] font-bold text-[18px]">Production Companies</h4>
+        <h4 className="text-[#c9173d] font-bold text-[16px] md:text-[18px]">Production Companies</h4>
         <>
           {getCompanies}
         </>
@@ -71,7 +71,7 @@ const Sidebar = ({isMovie, contentData}: Props) => {
       {
         isMovie ? (
           <div>
-            <h4 className="text-[#c9173d] font-bold text-[18px]">Crews</h4>
+            <h4 className="text-[#c9173d] font-bold text-[16px] md:text-[18px]">Crews</h4>
             <div className="grid gap-2">
               <>
                 {getCrew}
@@ -80,7 +80,7 @@ const Sidebar = ({isMovie, contentData}: Props) => {
           </div>
         ) : (
           <div>
-            <h4 className="text-[#c9173d] font-bold text-[18px]">Total Episodes</h4>
+            <h4 className="text-[#c9173d] font-bold text-[16px] md:text-[18px]">Total Episodes</h4>
             {`${number_of_episodes} episodes`}
           </div>
         )
