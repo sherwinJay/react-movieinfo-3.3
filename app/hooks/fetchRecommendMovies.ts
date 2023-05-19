c9d166d93bde66d50b3bfeb99190792664d16b78
sherwin.web.ph <sherwin.tripleonetech@gmail.com>
2023-05-19 19:40:32 +0800

    test update 15

--- app/hooks/fetchRecommendMovies.ts
+++ app/hooks/fetchRecommendMovies.ts
@@ -5,7 +5,7 @@ export async function fetchRecommendMovies(pageId: string | number, mediaType: s
   try {
     const res = await fetch(
       `${movieDbURL}/3/${mediaType}/${pageId}/recommendations?api_key=${process.env.MOVIE_DATABASE_ID}&language=en-US&page=1`,
-      // {cache: "no-store"}
+      {cache: "no-store"}
     );
     const recommendMovieList = await res.json()
   
--- app/hooks/getMovieData.ts
+++ app/hooks/getMovieData.ts
@@ -4,7 +4,7 @@ export async function getMovieData(pageId: string | number, mediaType: string){
   try {
     const res = await fetch(
       `${movieDbURL}/3/${mediaType}/${pageId}?api_key=${process.env.MOVIE_DATABASE_ID}&append_to_response=credits,videos`,
-        // {cache: "no-store"}
+        {cache: "no-store"}
     );
     
     const movieList = await res.json()
