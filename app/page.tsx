b26591dd468473e3e50c82e970d8d5e3890b0f1f
sherwin.web.ph <sherwin.tripleonetech@gmail.com>
2023-06-20 20:14:10 +0800

    update fectch home function

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import { movieDbURL } from '@/constant'
 import axios from 'axios';
 import { HomeCardData } from '@/types';
 import { HomeSection, Slider } from '@/components';
-import { fetchHomePageMovies } from '@/utils/fetchHomePageMovies';
 
 const inter = Inter({ subsets: ['latin'] })
+
+type MovieDataType = {
+  results: HomeCardData[]
+}
 
 export default async function Home() {
 
@@ -14,7 +17,7 @@ export default async function Home() {
   return (
     <>
       <Slider
-        movieData={popularMovie}
+        movieData={popularMovie.results}
       />
       <main className="w-[100%] p-[1.5em] mx-auto flex flex-col gap-3 lg:p-[3em] xl:w-[1200px]">
         <HomeSection
@@ -22,7 +25,7 @@ export default async function Home() {
           isMovie={true}
           template={'1'}
           imgCount={8}
-          categoryData={upcomingMovie}
+          categoryData={upcomingMovie.results}
         />
 
         <HomeSection 
@@ -30,7 +33,7 @@ export default async function Home() {
           isMovie={true}
           template={'2'}
           imgCount={7}
-          categoryData={nowPlayingMovie}
+          categoryData={nowPlayingMovie.results}
         />
 
         <HomeSection 
@@ -38,7 +41,7 @@ export default async function Home() {
           isMovie={false}
           template={'1'}
           imgCount={8}
-          categoryData={popularTV}
+          categoryData={popularTV.results}
         />
       </main>
     </>
@@ -50,18 +53,33 @@ async function getMoviesData(){
   // const movieHostName = `https://api.themoviedb.org/3`;
 
   // NOTE: by returning directly the results fetch could break if the other api end point is blank/error 
+
+  const fetchPopular = await fetch(`${movieDbURL}/3/movie/popular?api_key=${process.env.MOVIE_DATABASE_ID}&language=en-US&page=1`,{cache: "no-store"})
+  const fetchUpcoming = await fetch(`${movieDbURL}/3/movie/upcoming?api_key=${process.env.MOVIE_DATABASE_ID}&language=en-US&page=1`,{cache: "no-store"})
+  const fetchNowPlaying = await fetch(`${movieDbURL}/3/movie/now_playing?api_key=${process.env.MOVIE_DATABASE_ID}&language=en-US&page=1`,{cache: "no-store"})
+  const fetchPopularTV = await fetch(`${movieDbURL}/3/tv/popular?api_key=${process.env.MOVIE_DATABASE_ID}&language=en-US&page=1`,{cache: "no-store"})
  
+
   const [ 
     popularMovie,
     upcomingMovie,
     nowPlayingMovie,
     popularTV
-  ] = await Promise.all<HomeCardData[]>([
-    fetchHomePageMovies(`${movieDbURL}/3/movie/popular?api_key=${process.env.MOVIE_DATABASE_ID}&language=en-US&page=1`),
-    fetchHomePageMovies(`${movieDbURL}/3/movie/upcoming?api_key=${process.env.MOVIE_DATABASE_ID}&language=en-US&page=1`),
-    fetchHomePageMovies(`${movieDbURL}/3/movie/now_playing?api_key=${process.env.MOVIE_DATABASE_ID}&language=en-US&page=1`),
-    fetchHomePageMovies(`${movieDbURL}/3/tv/popular?api_key=${process.env.MOVIE_DATABASE_ID}&language=en-US&page=1`),
-  ])
+  ] = await Promise.all([
+    fetchPopular,
+    fetchUpcoming,
+    fetchNowPlaying,
+    fetchPopularTV
+
+  ]).then( values => {
+    return Promise.all<MovieDataType>(
+      values.map(response => {
+        return response.json()
+      })
+    )
+  }).catch(error => {
+    return []
+  })
 
   return {
     popularMovie,
