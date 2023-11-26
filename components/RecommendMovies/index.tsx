7f9b1b463f8247884eeca91225e79e8a3d9441a2
sherwin.web.ph <sherwin.tripleonetech@gmail.com>
2023-11-27 04:27:20 +0800

    replaced swr with react query fix

--- components/RecommendMovies/index.tsx
+++ components/RecommendMovies/index.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-import { movieDbImgURL } from '@/constant'
 import { RecommendData } from '@/types'
 import React, { useCallback, useEffect, useState } from 'react'
-import useSWR from 'swr'
 import RecommendCards from './RecommendCards'
 import clsx from 'clsx'
 import { normalizeImgixUrl } from '@/utils/imgixLoader'
@@ -25,11 +23,9 @@ const RecommendMovies = ({contentData, mediaType}: Props) => {
     contentData?.results && setBackground(`${normalizeImgixUrl(`/t/p/w1280${contentData?.results[0]?.backdrop_path}`)}`)
    
   }, [contentData?.results])
-  
 
   // change background when hovering to the recommendation thumbnails
   const pointerEvent = useCallback((imageId: string | null | undefined) => {
-    // console.log('render')
     if(background){
       setBackground(`${normalizeImgixUrl(`/t/p/w1280${imageId}`)}`)
     }
@@ -37,7 +33,6 @@ const RecommendMovies = ({contentData, mediaType}: Props) => {
   }, [background])
 
   const slicedRecommendData =  contentData?.results?.slice(0,7);
-
   const recommendMovieLists = slicedRecommendData?.map((movie) => (
     <RecommendCards
       key={movie.id}
@@ -49,20 +44,16 @@ const RecommendMovies = ({contentData, mediaType}: Props) => {
     />
   )) 
 
-
   // inline style for the recommendation container
   const recommendationBg = {
     backgroundImage: `linear-gradient(rgba(11,11,11,0.6),rgba(11,11,11,0.2)), url(${background})`,
   }
 
   return (
-    <div 
-      className={
-        clsx(
-          `font-satoshi bg-cover bg-center bg-no-repeat transition-all duration-500`,
-          contentData.results.length === 0 && '!bg-slate-900 !bg-none',
-        )
-      } 
+    <div className={clsx(
+        `font-satoshi bg-cover bg-center bg-no-repeat transition-all duration-500`,
+        contentData.results.length === 0 && '!bg-slate-900 !bg-none',
+      )} 
       style={recommendationBg}
     >
       <h2 className="mb-[20px] text-[20px] md:text-2xl font-bold pt-4 px-4 md:px-5">
