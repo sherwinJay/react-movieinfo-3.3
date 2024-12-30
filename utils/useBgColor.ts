f542884d39e0dfeee0ee5baa99c449a0c96274b1
sherwin.web.ph <sherwin.tripleonetech@gmail.com>
2024-12-31 02:17:29 +0800

    fixing internel error deployment

--- utils/useBgColor.ts
+++ utils/useBgColor.ts
@@ -4,7 +4,7 @@ import { useColor } from "color-thief-react"
 export const useBgColor = (imageUrl: string) => {
   const testImage = `${movieDbImgURL}/t/p/w300/${imageUrl}&api_key=${process.env.MOVIE_DATABASE_ID}`
   const testImage2 = `${movieDbImgURL}/t/p/w154${imageUrl}&api_key=${process.env.MOVIE_DATABASE_ID}`
-  const { data, loading, error } = useColor(testImage2, "hex", {
+  const { data, loading, error } = useColor(testImage, "hex", {
     crossOrigin: "anonymous",
   })
 
