3b5750b78f4ce23403091cc3a89f10bb8895f253
sherwin.web.ph <sherwin.tripleonetech@gmail.com>
2024-12-31 00:42:39 +0800

    added skeleton component

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
+        protocol: "https",
+        hostname: "image.tmdb.org",
       },
     ],
     // loader: 'custom',
