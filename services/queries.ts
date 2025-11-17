92f72388296d0430cb92a137d3a6fa7d8d3771a6
sherwin.web.ph <sherwin.tripleonetech@gmail.com>
2025-11-18 04:33:40 +0800

    added staletime on react query functions

--- services/queries.ts
+++ services/queries.ts
@@ -11,7 +11,7 @@ export function getSearchMovies(searchValue: string) {
   return useQuery({
     queryKey: ["shows", { searchValue }],
     queryFn: ({ signal }) => fetchSearchMovies({ searchValue, signal }),
-    staleTime: 10000,
+    staleTime: 1000,
   })
 }
 
@@ -19,7 +19,7 @@ export function getRecommendations(pageId: string | number, mediaType: string) {
   return useQuery({
     queryKey: ["recommendations", { pageId, mediaType }],
     queryFn: async () => await fetchRecommendMovies2({ pageId, mediaType }),
-    staleTime: 1000,
+    staleTime: 1000 * 60 * 10,
   })
 }
 
@@ -27,7 +27,7 @@ export function getPopularMovies(url: string) {
   return useQuery({
     queryKey: ["popularMovies", { url }],
     queryFn: async () => await fetchHomePageMovies(url),
-    staleTime: 1000,
+    staleTime: 1000 * 60 * 5,
   })
 }
 
@@ -35,7 +35,7 @@ export function getActorMovies(url: string) {
   return useQuery({
     queryKey: ["movie", { url }],
     queryFn: ({ signal }) => ({ url }),
-    staleTime: 10000,
+    staleTime: 1000 * 60 * 5,
   })
 }
 
@@ -43,7 +43,7 @@ export function getTrending() {
   return useQuery({
     queryKey: ["trendings"],
     queryFn: async () => await fetchTrending(),
-    staleTime: 1000,
+    staleTime: 1000 * 60 * 5,
   })
 }
 
@@ -54,6 +54,6 @@ export function getTrendingTrailers(
   return useQuery({
     queryKey: ["trendings", { pageId, mediaType }],
     queryFn: async () => await fetchTrendingTrailers({ pageId, mediaType }),
-    staleTime: 1000,
+    staleTime: 1000 * 60 * 5,
   })
 }
